7b05e4cdcbbeb89beb74869ed055402626ff273d
Sergi <sergimrtnz@gmail.com>
2018-02-13 16:43:34 +0100

    test CI

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,7 +1,7 @@
 <div id="wrapper">
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">hello wolrdies!</h1>
+            <h1 class="page-header">Hello wolrdies, this is CI!</h1>
         </div>
     </div>
 </div>       

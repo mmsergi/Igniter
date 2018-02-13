01c21bb384fa8528177d9d62fd7050d6d3e5d18a
Sergi <sergimrtnz@gmail.com>
2018-02-13 16:22:31 +0100

    first commit

---
+++ application/views/pages/home.php
@@ -0,0 +1,7 @@
+<div id="wrapper">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">hello wolrdies!</h1>
+        </div>
+    </div>
+</div>       
---
+++ application/views/templates/header.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <!-- Bootstrap Core CSS -->
+        <link href="<? echo base_url();?>vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    
+        <!-- MetisMenu CSS -->
+        <link href="<? echo base_url();?>vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+    
+        <!-- Custom CSS -->
+        <link href="<? echo base_url();?>dist/css/sb-admin-2.css" rel="stylesheet">
+    
+        <!-- Custom Fonts -->
+        <link href="<? echo base_url();?>vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        
+        <title>CodeIgniter Tutorial</title>
+    </head>
+        <body>
